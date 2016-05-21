e3a46158ecb1fb773bc9f531a6147ca35d651286
Mike Fleckenstein <mrfleck@hotmail.com>
2016-05-21 11:47:39 -0800

    Initial Commit

---
+++ src/TestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace TestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var mike = Resources.Resource.ResourceManager.GetString("test");
+            Console.WriteLine(mike);
+        }
+    }
+}
